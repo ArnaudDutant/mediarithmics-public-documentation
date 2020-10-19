4e46908e2cd1ff1e7ea735ae78120676b948be44
Arnaud Dutant <adutant@mediarithmics.com>
2020-10-19 17:53:51 +0200

    Update 2020-10-19T15:53:49.911Z

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
-const gulp = require('gulp');
-const run = require('gulp-run-command').default;
-const gulpLoadPlugins = require('gulp-load-plugins');
+const gulp = require("gulp");
+const run = require("gulp-run-command").default;
+const gulpLoadPlugins = require("gulp-load-plugins");
 
 const $ = gulpLoadPlugins();
 
-gulp.task('publish', () => {
-  console.log('Publish Gitbook (_book) to Github Pages');
-  return run('gitbook build --gitbook=3.2.3') 
-    && gulp.src('./_book/**/*')
-    .pipe($.ghPages({
-      origin: 'origin',
-      branch: 'gh-pages'
-    }));
+gulp.task("gitbook-build", run("gitbook build --gitbook=3.2.3"));
+
+gulp.task("gitbook-commit", () => {
+    return gulp.src("./_book/**/*").pipe(
+        $.ghPages({
+            origin: "origin",
+            branch: "gh-pages",
+        })
+    );
+});
+
+gulp.task("publish", () => {
+    console.log("Publish Gitbook (_book) to Github Pages");
+    return gulp.series("gitbook-build");
 });
