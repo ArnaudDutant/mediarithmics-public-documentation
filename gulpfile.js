f5ad77e82cdf4b79ae1309743ff48f9b5d589954
Arnaud Dutant <adutant@mediarithmics.com>
2020-10-21 10:48:19 +0200

    Update 2020-10-21T08:48:18.645Z

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const $ = gulpLoadPlugins();
 gulp.task("gitbook-build", run("gitbook build --gitbook=3.2.3"));
 
 gulp.task("gitbook-commit", () => {
-    return gulp.src(["./_book/**/*", "./_book/.gitbook/**/*"]).pipe(
+    return gulp.src(["./_book/**/*", "./_book/.*"]).pipe(
         $.ghPages({
             origin: "origin",
             branch: "gh-pages",
